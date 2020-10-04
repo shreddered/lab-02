2f2752f498c917b65e45eaf3ec020243f544b28a
Dmitry Kalinin <mitya17092001@gmail.com>
2020-10-04 14:36:17 +0300

    added demo

--- source/app.d
+++ source/app.d
@@ -1,6 +1,12 @@
-import std.stdio;
+import algorithms;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import std.math : exp;
+
+alias fun = (x) => (1 - x) * (1 - x) + exp(x);
+
+int main() {
+    auto searcher = new RandomSearcher!fun;
+    searcher.setInterval(-5, 2);
+    searcher.search(0.1);
+    return 0;
 }
