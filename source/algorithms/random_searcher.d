eb1c984f847e1d04ff71f5303a2da995d5b5d61e
Dmitry Kalinin <mitya17092001@gmail.com>
2020-10-04 15:28:15 +0300

    added n(p, q) table

--- source/algorithms/random_searcher.d
+++ source/algorithms/random_searcher.d
@@ -19,36 +19,69 @@ class RandomSearcher(alias func) : ISearcher {
 
     private void print(in double[] p, in double[] q, in double[][] mins) {
         // table header
-        writeln("+-------+-------+-------+-------+-------+-------",
-                "+-------+-------+-------+-------+-------+");
+        writeln("Точки минимума функции в зависимости от p и q");
+        writeln("+-------+---------+---------+---------+---------",
+                "+---------+---------+---------+---------+---------",
+                "+---------+");
         write("|  q\\P  ");
-        p.each!((a) => writef("| %-1.2f  ", a));
+        p.each!((a) => writef("|  %-2.2f   ", a));
         writeln('|');
-        writeln("+-------+-------+-------+-------+-------+-------",
-                "+-------+-------+-------+-------+-------+");
+        writeln("+-------+---------+---------+---------+---------",
+                "+---------+---------+---------+---------+---------",
+                "+---------+");
 
         foreach(i, seq; mins) {
             writef("| %-1.3f ", q[i]);
             foreach(j, elem; seq) {
-                writef("|%1.5f", elem);
+                writef("|%9,2.4f", elem);
             }
             writeln('|');
         }
         // table bottom
-        writeln("+-------+-------+-------+-------+-------+-------",
-                "+-------+-------+-------+-------+-------+");
+        writeln("+-------+---------+---------+---------+---------",
+                "+---------+---------+---------+---------+---------",
+                "+---------+");
     }
 
+    private void printN(in double[] p, in double[] q, in ulong[][] n) {
+        writeln("Зависимость N от p и q");
+        // table header
+        writeln("+-------+---------+---------+---------+---------",
+                "+---------+---------+---------+---------+---------",
+                "+---------+");
+        write("|  q\\P  ");
+        p.each!((a) => writef("|  %-2.2f   ", a));
+        writeln('|');
+        writeln("+-------+---------+---------+---------+---------",
+                "+---------+---------+---------+---------+---------",
+                "+---------+");
+
+        foreach(i, seq; n) {
+            writef("| %-1.3f ", q[i]);
+            foreach(j, elem; seq) {
+                writef("|%-9d", elem);
+            }
+            writeln('|');
+        }
+
+        // table bottom
+        writeln("+-------+---------+---------+---------+---------",
+                "+---------+---------+---------+---------+---------",
+                "+---------+");
+    }
     public override void search(const double eps) {
         enum double[] p = iota(0.9, 1.0, 0.01).array;
-        enum double[] q = iota(0.005, 0.105, 0.005).array; 
+        enum double[] q = iota(0.005, 0.105, 0.005).array;
 
         // computes 2D range (matrix) n = log(1 - p)/log(1 - q) at compile time
         // first mapping maps every q to another mapping
         // which maps every p to log(1 - p)/log(1 - q)
-        enum auto n = q.map!((q_) => p
-                .map!((p_) => cast(ulong) (log(1 - p_)/log(1 - q_)))); 
-
+        enum ulong[][] n = q.map!((q_) => p
+                .map!((p_) => cast(ulong) (log(1 - p_)/log(1 - q_)))
+                .array)
+                .array;
+        printN(p, q, n);
+        writeln;
         // helper function for generating n random numbers
         alias generateNRandomNumbers = (ulong _n) => generate(() => uniform!"[]"(_a, _b)).takeExactly(_n);
 
